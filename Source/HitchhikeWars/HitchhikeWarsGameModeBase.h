2c9b091e133f102ce7767f8672e5ee7b75165a46
Bogdan Panow <GoggiProsto@gmail.com>
2023-07-05 19:57:42 +0200

    Initialization

---
+++ Source/HitchhikeWars/HitchhikeWarsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HitchhikeWarsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HITCHHIKEWARS_API AHitchhikeWarsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
